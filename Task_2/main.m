520775ea3110da623456bd8a573180e218747192
Eugene Kirtaev <evgenijkirtaev@a.local>
2015-11-26 14:00:35 +0300

    Initial Commit

---
+++ Task_2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Task_2
+//
+//  Created by Евгений on 26.11.15.
+//  Copyright © 2015 Eugene Kirtaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
